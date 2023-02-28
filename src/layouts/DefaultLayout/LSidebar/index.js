05680d437c7d6f6dc36b3d15eabebede8731677d
xoaic <xoaic@xcc.one>
2023-02-28 10:44:13 +0700

    Initialize project v1.0

---
+++ src/layouts/DefaultLayout/LSidebar/index.js
@@ -0,0 +1,9 @@
+import styles from './LSidebar.module.scss';
+
+function LSidebar() {
+    return <aside className={styles.wrapper}>
+        <div>Left sidebar</div>
+    </aside>
+}
+
+export default LSidebar;
---
+++ src/layouts/DefaultLayout/index.js
@@ -0,0 +1,12 @@
+import LSidebar from "./LSidebar";
+
+function DefaultLayout() {
+    return (
+        <div>
+            <LSidebar />
+            <div>Test</div>
+        </div>
+    )
+}
+
+export default DefaultLayout;
